6f76c2c8996631310901b147c243c1e518813663
Miroslav Holec <mirek.holec@hotmail.com>
2017-06-29 10:53:54 +0200

    Dočištění hateoas

--- Demo02/Controllers/ApiController.cs
+++ Demo02/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Net;
 using Demo02.Lib.ApiModels.Base;
+using Demo02.Lib.ApiModels.Base.Hateoas;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
@@ -16,12 +17,14 @@ namespace Demo02.Controllers
 				Result = value
 			};
 
+			result.AddApiLinks(Url);
+
 			return base.Ok(result);
 		}
 
 		public new OkObjectResult Ok()
 		{
-			var result = new ResultBase()
+			var result = new ResultBase
 			{
 				Status = HttpStatusCode.OK
 			};
@@ -31,60 +34,82 @@ namespace Demo02.Controllers
 
 		public override CreatedAtRouteResult CreatedAtRoute(string routeName, object routeValues, object value)
 		{
-			var responseData = new ResultObject()
+			var result = new ResultObject
 			{
 				Status = HttpStatusCode.Created,
 				Result = value
 			};
 
-			return base.CreatedAtRoute(routeName, routeValues, responseData);
+			result.AddApiLinks(Url);
+
+			return base.CreatedAtRoute(routeName, routeValues, result);
 		}
 
 		public new NotFoundObjectResult NotFound()
 		{
-			var responseData = new ResultError()
+			var result = new ResultError
 			{
 				Status = HttpStatusCode.Created,
 			};
 
-			return base.NotFound(responseData);
+			return base.NotFound(result);
 		}
 
 		public NotFoundObjectResult NotFound(string message)
 		{
-			var responseData = new ResultError()
+			var result = new ResultError
 			{
 				Status = HttpStatusCode.Created,
 				Message = message
 			};
 
-			return base.NotFound(responseData);
+			return base.NotFound(result);
 		}
 
 		public override BadRequestObjectResult BadRequest(ModelStateDictionary modelState)
 		{
 			if (modelState == null)
+			{
 				throw new ArgumentNullException(nameof(modelState));
+			}
 
-			var responseData = new ResultError()
+			var result = new ResultError()
 			{
 				Status = HttpStatusCode.BadRequest,
 				Errors = new SerializableError(modelState),
 				Message = "See errors for more details"
 			};
 
-			return base.BadRequest(responseData);
+			return base.BadRequest(result);
 		}
 
 		public override BadRequestObjectResult BadRequest(object errorMessage)
 		{
-			var responseData = new ResultError()
+			var result = new ResultError
 			{
 				Status = HttpStatusCode.BadRequest,
 				Message = errorMessage.ToString()
 			};
 
-			return base.BadRequest(responseData);
+			return base.BadRequest(result);
+		}
+	}
+
+	public static class HateoasCommonExtensions
+	{
+		public static ResultObject AddApiLinks(this ResultObject resultObject, IUrlHelper urlHelper)
+		{
+			resultObject.Links.Add(
+				new Link
+				{
+					Href = urlHelper.Link("GetProducts", null),
+					Rel = "products",
+					Description = "Seznam produktů",
+					Method = "GET"
+				}
+			);
+
+			return resultObject;
 		}
 	}
 }
